e1e3936582206477b8cc1a117fa1bd727bb93460
baptist3 <186568477+baptist3-ng@users.noreply.github.com>
2026-03-05 22:39:56 +0100

    Ajout fonction question 10; Fix questions 11 & 12; Ajout questions 13 & 14

--- main.py
+++ main.py
@@ -206,29 +206,67 @@ vins = pd.read_csv('wine.csv')
 # Ca NE modifie PAS le fichier csv !
 
 vins = vins.dropna(subset=['Appelation'])
-for i in vins["Appelation"]:
-    print(i)
+#for i in vins["Appelation"]:
+#    print(i)
 
 
+##################################################################################
+# QUESTION GENERIQUE ?? ON FAIT UNE FONCTION QUI REPONDS A L'EXIGENCE DEMANDEE ? #
+##################################################################################
 # Question 10 : (Deja traitee plus haut en realite)
 for i in vins["Prix"]:
     if type(i) is not float:
         print("Error!")
 
+# SRC : https://stackoverflow.com/questions/2365411/convert-unicode-to-ascii-without-errors-in-python
+def convert_ascii(string):
+    return string.encode('ascii', 'ignore')
+
+#print("test\u202fbalbla")
+#print(convert_ascii("test\u202fbalbla"))
+
 # Question 11 : 
 
 moyenne_robert = vins.groupby("Appelation")["Parker"].mean()
-moyenne_robert["Parker"] = moyenne_robert["Parker"].fillna(0)
+#print(moyenne_robert)
+moyenne_robert = moyenne_robert.fillna(0)
+#print(moyenne_robert)
 
 #Question 12 :
 
 def get_moyenne(name):
     moyenne = vins.groupby("Appelation")[name].mean()
-    moyenne[name] = moyenne_robert[name].fillna(0)
+    moyenne = moyenne.fillna(0)
     return(moyenne)
+    #return vins.groupby("Appelation")[name].mean().fillna(0) # SI on est audacieux
 
 moyenne_robinson = get_moyenne("Robinson")
 moyenne_suckling = get_moyenne("Suckling")
+#print(moyenne_robinson)
+print(moyenne_suckling)
+
+## Question 13 :
+def replace(df,moyenne):
+    key = f"{moyenne.name}_bis"
+    df = df.merge(moyenne, on=["Appelation"], suffixes=('', '_bis'))
+    #print(df)
+    df[moyenne.name] = df[moyenne.name].fillna(df[key])
+    #print(df)
+    df = df.drop(columns=[key])
+    return df
+
+vins = replace(vins,moyenne_robert)
+vins = replace(vins,moyenne_robinson)
+vins = replace(vins,moyenne_suckling)
+print(vins)
+
+## Question 14 :
+vins = pd.get_dummies(vins, prefix='App', dtype = int)
+print(vins)
+print(vins.shape) # OK selon le sujet
+print(vins.dtypes) # Valeurs bien numeriques
+
+
 
 # Tests
 # print(informations(getsoup("https://www.millesima.fr/chateau-gloria-2016.html")))
